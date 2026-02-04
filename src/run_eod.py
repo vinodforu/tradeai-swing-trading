d0f9b4ed43b1ac781ed7bb003bfa43d1a783d20d
Vinod Satyanarayana <vinodforu@gmail.com>
2026-02-04 23:49:50 +0530

    changes to eod

--- src/run_eod.py
+++ src/run_eod.py
@@ -1,6 +1,6 @@
-from fetch_data import fetch_eod_data
-from indicators import add_indicators
-from screeners import pullback_screener
+from src.fetch_data import fetch_eod_data
+from src.indicators import add_indicators
+from src.screeners import pullback_screener
 
 def run_eod():
     data = fetch_eod_data()
